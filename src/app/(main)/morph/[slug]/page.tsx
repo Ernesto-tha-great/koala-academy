c1281a1b64403463b7f7e6437fa81c979ff5b81e
Ernest Nnamdi <ernestnamdi66@gmail.com>
2025-01-14 14:11:11 +0100

    try again

--- src/app/(main)/morph/[slug]/page.tsx
+++ src/app/(main)/morph/[slug]/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { notFound } from "next/navigation";
 import { getMorphSection } from "@/lib/morph/content";
 import Link from "next/link";
